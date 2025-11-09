7859f96ee91a675d4cada4fcf0c31043e14070e5
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-11-09 18:34:36 +0800

    adding debug macro

--- ax_process_utils/src/ax_process_utils.cpp
+++ ax_process_utils/src/ax_process_utils.cpp
@@ -9,9 +9,17 @@
 #include <mutex>
 #include <vector>
 
+#define DEBUG_BUILD 0
+
 #define LOG_TAG "AxProcessUtils"
-#define ALOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
-#define ALOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
+
+#if DEBUG_BUILD
+  #define ALOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+  #define ALOGV(...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
+#else
+  #define ALOGE(...) ((void)0)
+  #define ALOGV(...) ((void)0)
+#endif
 
 namespace axion::process {
 
