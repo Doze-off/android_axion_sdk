7e17b631b09c90fc2065301607795efff4ae67e2
Rve27 <rve27github@gmail.com>
2026-01-11 06:26:22 +0700

    axion_sdk: Import Material 3 v1.5.0-alpha11
    Change-Id: I88af2fe8860ee517535e80d895350296080fdb9a
    Signed-off-by: Rve27 <rve27github@gmail.com>

---
+++ scripts/material3.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+ARTIFACT="material3-android"
+VERSION=""
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -v|--version)
+            VERSION="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
+
+if [[ -z "$VERSION" ]]; then
+    echo "Usage: $0 -v <version>"
+    exit 1
+fi
+
+URL="https://dl.google.com/dl/android/maven2/androidx/compose/material3/$ARTIFACT/$VERSION"
+
+exts=( ".aar" ".pom" ".module" "-sources.jar" "-samples-sources.jar" )
+sums=( "" ".md5" ".sha1" ".sha256" ".sha512" )
+
+echo "Downloading Material 3 artifacts - version $VERSION"
+
+for ext in "${exts[@]}"; do
+    for sum in "${sums[@]}"; do
+        filename="$ARTIFACT-$VERSION$ext$sum"
+        link="$URL/$filename"
+
+        printf "%-55s " "$filename"
+
+        if wget -q --show-progress "$link"; then
+            echo ""
+        else
+            echo "Not found"
+        fi
+    done
+done
